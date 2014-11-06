73c3ed747620f82e0453958a5a028e99f896ff0b
slothy <slothyrulez@gmail.com>
2014-11-06 22:08:48 +0100

    Rework upper pinouts

--- IoTPy/pyuper/pinouts.py
+++ IoTPy/pyuper/pinouts.py
@@ -1,43 +1,46 @@
-from IoTPy.pyuper.utils import IoTPy_APIError
+from IoTPy.core.utils import IoTPy_APIError
+from IoTPy.core.pinouts import CAP_RESERVED, CAP_GPIO, CAP_ADC, CAP_PWM, CAP_SPI
+from IoTPy.core.pinouts import IoParams, IoPinout
 
-CAP_RESERVED = 0x0
-CAP_GPIO     = 0x1
-CAP_ADC      = 0x2
-CAP_PWM      = 0x4
-CAP_SPI      = 0x8
+# CAP_RESERVED = 0x0
+# CAP_GPIO     = 0x1
+# CAP_ADC      = 0x2
+# CAP_PWM      = 0x4
+# CAP_SPI      = 0x8
 
-class IoParams:
-    """
-    Class describing IO pin.
 
-    :param int capabilities: Integer describing pin capabilities. Can be a combination of CAP_CAP_RESERVED, CAP_GPIO, CAP_ADC, CAP_PWM and CAP_SPI.
-    :param int pinID: Pin ID.
-    :param str name: Pin alias name.
-    :param list extra: Extra data for pin capabilities.
-    """
+# class IoParams:
+#     """
+#     Class describing IO pin.
+#
+#     :param int capabilities: Integer describing pin capabilities. Can be a combination of CAP_CAP_RESERVED, CAP_GPIO, CAP_ADC, CAP_PWM and CAP_SPI.
+#     :param int pinID: Pin ID.
+#     :param str name: Pin alias name.
+#     :param list extra: Extra data for pin capabilities.
+#     """
+#
+#     def __init__(self, capabilities, pinID, name, extra=None):
+#         self.capabilities = capabilities
+#         self.pinID = pinID
+#         self.pinName = name
+#         self.extra = extra
 
-    def __init__(self, capabilities, pinID, name, extra=None):
-        self.capabilities = capabilities
-        self.pinID = pinID
-        self.pinName = name
-        self.extra = extra
-
-class IoPinout(dict):
-    """
-    A dictionary consisting of integer keys and :class:`IoParams` values which describe board pin mapping and capabilities.
-    """
-
-    def __init__(self, *args, **kw):
-        super(IoPinout,self).__init__(*args, **kw)
-        for key in self:
-            if not isinstance(key, int) or not isinstance(self[key], IoParams):
-                raise IoTPy_APIError("IoPinout must consist of integer keys and IoParams values.")
-
-    def __delitem__(self, key):
-        raise IoTPy_APIError("IoPinout can not be modified.")
-
-    def __setitem__(self, key, value):
-        raise IoTPy_APIError("IoPinout can not be modified.")
+# class IoPinout(dict):
+#     """
+#     A dictionary consisting of integer keys and :class:`IoParams` values which describe board pin mapping and capabilities.
+#     """
+#
+#     def __init__(self, *args, **kw):
+#         super(IoPinout,self).__init__(*args, **kw)
+#         for key in self:
+#             if not isinstance(key, int) or not isinstance(self[key], IoParams):
+#                 raise IoTPy_APIError("IoPinout must consist of integer keys and IoParams values.")
+#
+#     def __delitem__(self, key):
+#         raise IoTPy_APIError("IoPinout can not be modified.")
+#
+#     def __setitem__(self, key, value):
+#         raise IoTPy_APIError("IoPinout can not be modified.")
 
 
 UPER1_PINOUT = IoPinout({
